05d04ecdaf96b32216e849e9169247ddd590865e
60138546 <1011888891@qq.com>
2024-02-27 18:00:07 +0800

    no message

---
+++ Model/Article.cs
@@ -0,0 +1,17 @@
+﻿namespace HexoBlog.Model
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Updated { get; set; }
+        public bool Comments { get; set; }
+        public string Path { get; set; }
+        public string Excerpt { get; set; }
+        public string Content { get; set; }
+        public string More { get; set; }
+        public List<CategoryItem> Categories { get; set; }
+        public List<CategoryItem> Tags { get; set; }
+    }
+}
---
+++ Service/ArticleService.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ganss.Xss;
+using HexoBlog.Model;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace HexoBlog.Service
+{
+    internal class ArticleService
+    {
+
+
+        private string articleContent;
+
+
+        public async Task GetArticleContentAsync(string path)
+        {
+            try
+            {
+                try
+                {
+                    //var response = await Http.GetAsync($"articles/{Path}");
+                    //if (response.IsSuccessStatusCode)
+                    //{
+                    //    var content = await response.Content.ReadFromJsonAsync<Article>();
+                    //    articleContent = HtmlSanitizer.Sanitize(content.Content); // 假设你有一个HtmlSanitizer类来清理HTML内容
+                    //}
+                    //else
+                    //{
+                    //    // 处理错误情况
+                    //}
+                }
+                catch (HttpRequestException ex)
+                {
+                    // 处理异常
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
