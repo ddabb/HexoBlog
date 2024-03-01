ba5c93d4c127c96befcf1946a8059778365ad7bf
60138546 <1011888891@qq.com>
2024-03-01 17:24:59 +0800

    no message

--- Service/ClassifyService.cs
+++ Service/ClassifyService.cs
@@ -3,6 +3,10 @@ using RestSharp;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using Newtonsoft.Json;
+using Microsoft.AspNetCore.Components;
+
+
+using System.Net;
 namespace HexoBlog.Service
 {
     public class ClassifyService
@@ -48,10 +52,40 @@ namespace HexoBlog.Service
 
         }
 
-        public async Task LoadTagArticleAsync(string json)
+        public async Task LoadTagArticleAsync(NavigationManager nav)
         {
             try
             {
+
+                // 获取当前的 URI
+                var uri = nav.Uri;
+                var type = "tags";
+                var json = "";
+
+
+                //// 解析查询字符串
+                //var queryParameters = new Uri(uri).Query;
+
+                //// 尝试从查询字符串中获取params参数
+                //if (queryParameters.TryGetValue("params", out var paramsValues) && paramsValues.Count > 0)
+                //{
+                //    string encodedJson = paramsValues[0]; // 获取第一个params值
+
+                //    // 对URL编码的JSON字符串进行解码
+                //    string decodedJson = System.Net.WebUtility.UrlDecode(encodedJson);
+
+                //    // 反序列化JSON字符串为对象
+                //    var articleParams = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, string>>(decodedJson);
+
+                //    if (articleParams != null && articleParams.ContainsKey("name") && articleParams.ContainsKey("type"))
+                //    {
+                //        Name = articleParams["name"];
+                //        string type = articleParams["type"];
+                //        string decodedString = WebUtility.UrlDecode(Name);
+                //        json = decodedString.Split('/').Last();
+
+                //    }
+                //}
 
                 var request = new RestRequest($"/api/tags/{json}", Method.Get);
 
